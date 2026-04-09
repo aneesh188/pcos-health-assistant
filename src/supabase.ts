7151e41b2ea03e377f7b3eb00c401324fd3fd7d9
aneesh188 <aneeshbh99@gmail.com>
2026-04-09 07:10:18 +0530

    Fix supabase env variables

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://kptvgnmpclvswhejlttg.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_aowlG6USWkVSstyFtDvTEg_1U3LpvWZ'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
